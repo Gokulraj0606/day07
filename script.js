f69d3b783b3699b7f6b9f5d061cf0f5154957739
Gokulraj0606 <108870590+Gokulraj0606@users.noreply.github.com>
2022-08-06 16:51:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,9 @@ request.open("Get","https://raw.githubusercontent.com/rvsp/restcountries-json-da
 request.send();
 request.onload=function(){
   var result=JSON.parse(request.response);
-  
 
-  var res=result.map((ele)=>ele.name)
-  res.forEach(ele=>{
-    console.log(ele)
-  })
-  var abc=result.map((ele0)=>ele0.capital)
-  abc.forEach(ele0=>{
-    console.log(ele0)
-  })
-
-
-
-
-  var ret=result.map((ele1)=>ele1.flag)
-  ret.forEach(ele1=>{
-    console.log(ele1)
-  })
-  
+  var res=result.map((ele)=>ele.population).reduce((acc,ele)=>acc+ele);
+  console.log(res)
+ 
  
 }
